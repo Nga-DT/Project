3333db6fccaa866eefc620d11f2d37e5ce92ab74
Nga3008 <164482621+Nga3008@users.noreply.github.com>
2024-05-04 00:13:17 +0700

    Update project.sql

--- project.sql
+++ project.sql
@@ -37,7 +37,8 @@ WITH pct AS
   
 min_max AS
   (SELECT Q1-1.5*IQR AS min_value, Q3+1.5*IQR AS max_value FROM pct),
-  outlier AS
+  
+outlier AS
   (SELECT * FROM customer_shopping_data 
   WHERE quantity < (SELECT min_value FROM min_max ) 
   OR quantity > (SELECT max_value FROM min_max))
@@ -79,10 +80,10 @@ WITH order_ranking AS
 SELECT invoice_year, category
 FROM order_ranking
 WHERE stt<=3
--- Nhận xét: Food & Beverage, Clothing, Cosmetics là 3 sản phẩm bán chạy nhất trong cả 3 năm 2021,2022,2023.
+-- Nhận xét: Clothing, Cosmetics, Food & Beverage là 3 sản phẩm bán chạy nhất trong cả 3 năm 2021,2022,2023.
 
   -- Cohort analysis
-CREATE VIEW vw_cohort AS	
+CREATE OR REPLACE VIEW vw_cohort AS	
 	(WITH first_purchase_date AS	
 		(SELECT 
 		customer_id,
@@ -90,13 +91,16 @@ CREATE VIEW vw_cohort AS
 		MIN(invoice_date) OVER(PARTITION BY customer_id ) AS first_date,
 		invoice_date
 		FROM public.customer_shopping_data),
+	first_purchase_date_2023 AS
+		(SELECT * FROM first_purchase_date
+		 WHERE EXTRACT (YEAR FROM first_date)=2023),
 	cohort_index AS
 		(SELECT customer_id, amount,
 		TO_CHAR (first_date,'YYYY/MM') AS cohort_date,
 		invoice_date,
 		(EXTRACT(YEAR FROM invoice_date) - EXTRACT (YEAR FROM first_date))*12 + EXTRACT(MONTH FROM invoice_date) - 
 		EXTRACT (MONTH FROM first_date) +1  AS index
-		FROM first_purchase_date)
+		FROM first_purchase_date_2023)
 	SELECT cohort_date,
 	index,
 	COUNT (DISTINCT customer_id) AS user_count,
@@ -108,45 +112,23 @@ CREATE VIEW vw_cohort AS
 
 SELECT 
   cohort_date,
-  SUM(CASE WHEN index=1 THEN customer_count ELSE 0 END) AS m1,
-  SUM(CASE WHEN index=2 THEN customer_count ELSE 0 END) AS m2,
-  SUM(CASE WHEN index=3 THEN customer_count ELSE 0 END) AS m3,
-  SUM(CASE WHEN index=4 THEN customer_count ELSE 0 END) AS m4,
-  SUM(CASE WHEN index=5 THEN customer_count ELSE 0 END) AS m5,
-  SUM(CASE WHEN index=6 THEN customer_count ELSE 0 END) AS m6,
-  SUM(CASE WHEN index=7 THEN customer_count ELSE 0 END) AS m7,
-  SUM(CASE WHEN index=8 THEN customer_count ELSE 0 END) AS m8,
-  SUM(CASE WHEN index=9 THEN customer_count ELSE 0 END) AS m9,
-  SUM(CASE WHEN index=10 THEN customer_count ELSE 0 END) AS m10,
-  SUM(CASE WHEN index=11 THEN customer_count ELSE 0 END) AS m11,
-  SUM(CASE WHEN index=12 THEN customer_count ELSE 0 END) AS m12,
-  SUM(CASE WHEN index=13 THEN customer_count ELSE 0 END) AS m13,
-  SUM(CASE WHEN index=14 THEN customer_count ELSE 0 END) AS m14,
-  SUM(CASE WHEN index=15 THEN customer_count ELSE 0 END) AS m15,
-  SUM(CASE WHEN index=16 THEN customer_count ELSE 0 END) AS m16,
-  SUM(CASE WHEN index=17 THEN customer_count ELSE 0 END) AS m17,
-  SUM(CASE WHEN index=18 THEN customer_count ELSE 0 END) AS m18,
-  SUM(CASE WHEN index=19 THEN customer_count ELSE 0 END) AS m19,
-  SUM(CASE WHEN index=20 THEN customer_count ELSE 0 END) AS m20,
-  SUM(CASE WHEN index=21 THEN customer_count ELSE 0 END) AS m21,
-  SUM(CASE WHEN index=22 THEN customer_count ELSE 0 END) AS m22,
-  SUM(CASE WHEN index=23 THEN customer_count ELSE 0 END) AS m23,
-  SUM(CASE WHEN index=24 THEN customer_count ELSE 0 END) AS m24,
-  SUM(CASE WHEN index=25 THEN customer_count ELSE 0 END) AS m25,
-  SUM(CASE WHEN index=26 THEN customer_count ELSE 0 END) AS m26,
-  SUM(CASE WHEN index=27 THEN customer_count ELSE 0 END) AS m27,
-  SUM(CASE WHEN index=28 THEN customer_count ELSE 0 END) AS m28,
-  SUM(CASE WHEN index=29 THEN customer_count ELSE 0 END) AS m29,
-  SUM(CASE WHEN index=30 THEN customer_count ELSE 0 END) AS m30,
-  SUM(CASE WHEN index=31 THEN customer_count ELSE 0 END) AS m31,
-  SUM(CASE WHEN index=32 THEN customer_count ELSE 0 END) AS m32,
-  SUM(CASE WHEN index=33 THEN customer_count ELSE 0 END) AS m33,
-  SUM(CASE WHEN index=34 THEN customer_count ELSE 0 END) AS m34,
-  SUM(CASE WHEN index=35 THEN customer_count ELSE 0 END) AS m35,
-  SUM(CASE WHEN index=36 THEN customer_count ELSE 0 END) AS m36
+  SUM(CASE WHEN index=1 THEN user_count ELSE 0 END) AS m1,
+  SUM(CASE WHEN index=2 THEN user_count ELSE 0 END) AS m2,
+  SUM(CASE WHEN index=3 THEN user_count ELSE 0 END) AS m3,
+  SUM(CASE WHEN index=4 THEN user_count ELSE 0 END) AS m4,
+  SUM(CASE WHEN index=5 THEN user_count ELSE 0 END) AS m5,
+  SUM(CASE WHEN index=6 THEN user_count ELSE 0 END) AS m6,
+  SUM(CASE WHEN index=7 THEN user_count ELSE 0 END) AS m7,
+  SUM(CASE WHEN index=8 THEN user_count ELSE 0 END) AS m8,
+  SUM(CASE WHEN index=9 THEN user_count ELSE 0 END) AS m9,
+  SUM(CASE WHEN index=10 THEN user_count ELSE 0 END) AS m10,
+  SUM(CASE WHEN index=11 THEN user_count ELSE 0 END) AS m11,
+  SUM(CASE WHEN index=12 THEN user_count ELSE 0 END) AS m12
   FROM public.vw_cohort
   GROUP BY cohort_date
   ORDER BY cohort_date
+
+
   -- Ai là khách hàng tốt nhất, phân tích dựa vào RFM 
 
 
